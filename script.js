9bb26d11b72c9c59ce530dd0c4e68ac4f79cc51e
cas57 <127038483+cas57@users.noreply.github.com>
2023-03-10 17:13:58 -0500

    variable changes
    calendar - product
    newEventModal - newDeadlineModal

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let nav = 0;
 let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : []
 
-const calendar = document.getElementById('calendar');
+const product = document.getElementById('product');
 //declare the eventModal so its visible
-const newEventModal = document.getElementById('newEventModal');
+const newDeadlineModal = document.getElementById('newDeadlineModal');
 //func for deleting the modal
 const deleteEventModal = document.getElementById('deleteEventModal');
 //create another const for the backdrop
@@ -20,7 +20,7 @@ function openModal(date) { clicked = date;
     if (eventForDay) {
         document.getElementById('eventText').innerText = eventForDay.title; deleteEventModal.style.display = 'block';
     } else {
-        newEventModal.style.display = 'block';
+        newDeadlineModal.style.display = 'block';
     }
     backDrop.style.display = 'block';
 }
@@ -54,7 +54,7 @@ const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 //calling for the current/relevant month name
 document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
 
-calendar.innerHTML = '';
+product.innerHTML = '';
 
 for(let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement('div'); 
@@ -90,7 +90,7 @@ for(let i = 1; i <= paddingDays + daysInMonth; i++) {
 //func for the cancel button
 function closeModal () {
  eventTitleInput.classList.remove('error'); 
- newEventModal.style.display = 'none'; 
+ newDeadlineModal.style.display = 'none'; 
  deleteEventModal.style.display = 'none'; 
  backDrop.style.display = 'none'; 
  eventTitleInput.value = '';
