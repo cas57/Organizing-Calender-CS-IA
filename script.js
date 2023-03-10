0cdedf82ae9a4ca57b24ac13e3ca627874233d7b
cas57 <127038483+cas57@users.noreply.github.com>
2023-03-10 17:36:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 function openModal(date) { clicked = date;
     const eventForDay = events.find(e => e.date === clicked);
     if (eventForDay) {
-        document.getElementById('eventText').innerText = eventForDay.title; deleteEventModal.style.display = 'block';
+        document.getElementById('eventText').innerText = eventForDay.title; 
+        deleteEventModal.style.display = 'block';
     } else {
         newEventModal.style.display = 'block';
     }
